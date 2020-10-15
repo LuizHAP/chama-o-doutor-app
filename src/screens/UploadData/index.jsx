35162ce1283dfc4d45cabf6be14518bf125e72f0
Luiz Pansarini <luiz.pansarini@uaubox.com.br>
2020-10-14 23:23:27 -0300

    [WIP] test upload data

--- src/screens/UploadData/index.jsx
+++ src/screens/UploadData/index.jsx
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { RectButton } from "react-native-gesture-handler";
 
-import axios from 'axios'
+import axios from "axios";
 
 import AsyncStorage from "@react-native-community/async-storage";
 
@@ -15,21 +15,26 @@ const UploadData = (props) => {
   const handleUploadData = async () => {
     try {
       const keys = await AsyncStorage.getAllKeys();
-      const result = await AsyncStorage.multiGet(keys);
+      const result = await JSON.parse(AsyncStorage.multiGet(keys);
       const scriptURL =
         "https://script.google.com/macros/s/AKfycbxcCaNLfucf8u_NyJc9Kzzw1rsGUGx-Nov2xMPU1n8wSRy4fvGK/exec";
-      result.map(async (req) => req.forEach(await axios
-        .get(scriptURL, { params })
-        .then(async (r) => {
-          await AsyncStorage.clear();
-          navigate("Inicial");
+      result.map((req) =>
+        req.forEach(async () => {
+          console.warn(req);
+          await axios
+            .get(scriptURL, { params: req })
+            .then(async (r) => {
+              await AsyncStorage.clear();
+              navigate("Inicial");
+            })
+            .catch((e) => {
+              alert("Erro:", e.message);
+            });
         })
-        .catch((e) => {
-          alert("Erro:", e.message);
-        })))
-      } catch(e){
-        alert("Erro:", e.message)
-      }
+      );
+    } catch (e) {
+      alert("Erro:", e.message);
+    }
   };
 
   return (
